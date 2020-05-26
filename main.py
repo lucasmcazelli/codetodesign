f5bd32c80f5f45c02b004dc90a2ff4e70e87499a
lucasmcazelli <63100230+lucasmcazelli@users.noreply.github.com>
2020-05-26 20:24:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, send_file
+
+import numpy as np
+from PIL import Image
+import io
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+@app.route('/api/b')
+def array():
+    '''
+    generate image from numpy.array using PIL.Image
+    and send without saving on disk using io.BytesIO'''
+
+    arr = np.array([
+        [255, 255,   0,   0,   0,   0,   0,   0, 255, 255],
+        [255,   0, 255, 255, 255, 255, 255, 255,   0, 255],
+        [  0, 255, 255, 255, 255, 255, 255, 255, 255,   0],
+        [  0, 255, 255,   0, 255, 255,   0, 255, 255,   0],
+        [  0, 255, 255, 255, 255, 255, 255, 255, 255,   0],
+        [  0, 255, 255, 255, 255, 255, 255, 255, 255,   0],
+        [  0, 255,   0, 255, 255, 255, 255,   0, 255,   0],
+        [  0, 255, 255,   0,   0,   0,   0, 255, 255,   0],
+        [255,   0, 255, 255, 255, 255, 255, 255,   0, 255],
+        [255, 255,   0,   0,   0,   0,   0,   0, 255, 255],
+    ])
+
+    img = Image.fromarray(arr.astype('uint8')) # convert arr to image
+
+    file_object = io.BytesIO()   # create file in memory 
+    img.save(file_object, 'PNG') # save as PNG in file in memory
+    file_object.seek(0)          # move to beginning of file
+                                 # so send_file() will read data from beginning of file
+
+    return send_file(file_object,  mimetype='image/png')
+
+app.run(host='0.0.0.0', port=8080)
+app.run()
